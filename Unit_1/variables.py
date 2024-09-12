4cdbd3d8912391ec6204b43a4b772364e43c1c76
Elianacobo <eboyar@dragonwork.com>
2024-09-12 13:56:10 +0000

    Created new files

---
+++ Unit_1/variables.py
@@ -0,0 +1,18 @@
+print("Good morning class!")
+
+# this is a comment. It is used for leaving comments in your code.
+
+# Comments are ignored by the computer.
+# Comments are used to leave notes for other people to read.
+
+x = 17
+y = 25
+print(x + y)
+
+print(x*y) # multiplication
+print(x/y) # division
+print(x-y) # subtracton
+print(x^y) # exponents
+
+thisIsAVariable = "Hello everyone! Welcome to python."
+print(thisIsAVariable)
