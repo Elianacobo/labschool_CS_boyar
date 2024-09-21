1211654a0ae8650889334275aa13a56cb10f38e2
Elianacobo <eboyar@dragonwork.com>
2024-09-21 14:25:36 -0400

    Eliana's Concatenation and fString Homework

---
+++ Unit_1/concatenationfstrings.py
@@ -0,0 +1,72 @@
+greeting = "Hello"
+name = "Jewel"
+welcome = "Welcome"
+w = "welcome"
+where = "to computer science"
+exclemationMark = "!"
+space = " "
+u = "you"
+r = "are"
+at = "at"
+stressful = "college aplications"
+ggame = "good game"
+elianaHobby = "sew"
+going = "going"
+doing = "doing"
+what = "what"
+my = "my"
+dad = "father"
+comma = ","
+eliana = "Eliana"
+n = "name"
+p = "is"
+elianaAge = "17"
+jewelAge = "9"
+questionMark = "?"
+how = "how"
+# 10 concatenation
+
+a = greeting+space+name+exclemationMark
+print(a)
+b = welcome+space+where+exclemationMark
+print(b)
+c = name+space+u+space+r+space+welcome+exclemationMark
+print(c)
+d = name+comma+space+u+space+r+space+going+space+where+exclemationMark
+print(d)
+e = name+comma+space+how+space+r+space+u+questionMark
+print(e)
+f = name+space+what+space+r+space+u+space+doing+questionMark+exclemationMark+questionMark
+print(f)
+g = name+space+u+space+r+space+my+space+dad+exclemationMark
+print(g)
+h = greeting+space+name+comma+space+my+space+n+space+p+space+eliana+exclemationMark
+print(h)
+i = u+space+r+space+w+exclemationMark
+print(i)
+j = u+space+r+space+doing+space+what+questionMark+questionMark+questionMark
+print(j)
+
+
+# 10 fString
+
+aa = f"My name is {eliana} and my dog's name is {name}"
+print(aa)
+bb = f"I am {elianaAge} years old and {name} is {jewelAge} years old."
+print(bb)
+cc = f"I like to {elianaHobby}, but I can't right now"
+print(cc)
+dd = f"I am doing homework while {name} is sleeping."
+print(dd)
+ee = f"My name is {eliana} and it starts with the letter E!"
+print(ee)
+ff = f"{what} are you {doing}?"
+print(ff)
+gg = f"gg means {ggame}."
+print(gg)
+hh = f"Something that is very stressful is {stressful}."
+print(hh)
+ii = f"I am {eliana} and I am bored of typing!"
+print(ii)
+jj = f"This is the last fString {eliana} has to type. (For Now!)"
+print(jj)
