9e663cbe4f8942d7c594d0fcb8162eee6479d355
Elianacobo <eboyar@dragonwork.com>
2024-09-23 09:56:17 -0400

    escape characters

---
+++ f-stringsescapecharaters.py
@@ -0,0 +1,17 @@
+
+#Example 1
+palpatineAnnouncement = "In order to ensure the secureity and continue stability,"
+palpatineAnnouncement2 = "the Repiblic will be RE-organized, into the first GALACTIC EMPIRE!"
+palpatineAnnouncement3 = "For a safe and SECURE... SOCIETY!"
+#print this out as three lines using f-strings and escape characters
+palpatine = f"{palpatineAnnouncement}\n{palpatineAnnouncement2}\n{palpatineAnnouncement3}"
+print(palpatine)
+
+print()
+
+#Example 2
+andrews = "That's five compartments!"
+andrews2 = "She can stay afloat with the first 4 compartments breached, but not five. NOT FIVE."
+#print this out as two lines using f-stings and escape characters
+together = f"{andrews}\n{andrews2}"
+print(together)
