878cc608649bc5351415f5b4fe8bec798ef02378
Elianacobo <eboyar@dragonwork.com>
2024-09-25 12:40:52 -0400

    f-Strings and escape characters

---
+++ elianasbooklines.py
@@ -0,0 +1,21 @@
+#Take a favorite movie scene or song and break it apart into variables.
+
+#Restructure it using variables:
+#- concatenate with escape characters
+#OR
+#- f-Strings with escape characters
+
+#You must have AT LEAST 8 - 12 lines of lyric lines or movie quote lines.
+
+first = "The  Circus  McGurkus! The World's Greatest Show"
+second = "On  the  face  of  the  earth, or  wherever you  go!"
+third = "The  Circus McGurkus! The cream  of  the cream!"
+fourth = "The Circus McGurkus! The Circus Supreme!"
+fith = "The Circus McGurkus! Colossal! Stupendous! Astounding! Fantastic! Terrific! Tremendous!"
+sixth = "I will bring  in my  acrobats, jugglers and clowns From a  thousand and  thirty-three faraway towns"
+seventh = "To the  place that  youll  see  them  in, ladies  and  gents, Right behind Sneelocks Store, in the  Great McGurk tents!" 
+eighth = "And I don't suppose old  Mr.  Sneelock will mind" 
+ninth = "When he suddenly has a  big circus behind..."
+
+ifIRanTheCircus = f'{first}\n{second}\n\n{third}\n{fourth}\n{fith}\n\n{sixth}\n{seventh}\n{eighth}\n{ninth}'
+print(ifIRanTheCircus)
